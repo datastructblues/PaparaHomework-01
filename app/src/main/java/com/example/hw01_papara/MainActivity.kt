baebe770c3dfd8498d2ebd92a1caff9e67df1bf3
Can Usta <canusta_45@hotmail.com>
2024-05-19 11:26:34 +0300

    Libraries added in libs.versions.toml and the location of the application class has been changed.

--- app/src/main/java/com/example/hw01_papara/MainActivity.kt
+++ app/src/main/java/com/example/hw01_papara/MainActivity.kt
@@ -9,8 +9,9 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import com.example.hw01_papara.ui.theme.Hw01paparaTheme
+import dagger.hilt.android.AndroidEntryPoint
 
-
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ app/src/main/java/com/example/hw01_papara/di/AppModule.kt
@@ -0,0 +1,26 @@
+package com.example.hw01_papara.di
+
+import com.example.hw01_papara.data.repository.AuthRepository
+import com.google.firebase.auth.FirebaseAuth
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideFirebaseAuth(): FirebaseAuth {
+        return FirebaseAuth.getInstance()
+    }
+
+    @Provides
+    @Singleton
+    fun provideAuthRepository(auth: FirebaseAuth): AuthRepository {
+        return AuthRepository(auth)
+    }
+}
