6af0b6cc6804493873d0553f7f145a074c8021b6
Umut Fidan <contact.umutfidan@gmail.com>
2024-05-20 16:14:04 +0300

    edteToEdge added.

--- app/src/main/java/com/example/hw01_papara/MainActivity.kt
+++ app/src/main/java/com/example/hw01_papara/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.hw01_papara
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -13,6 +14,7 @@ import com.example.hw01_papara.ui.theme.Hw01paparaTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContent {
             Hw01paparaTheme {
                 Surface(
