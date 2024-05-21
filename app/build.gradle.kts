76c688f7603e6245385fd3a75659fb28bc705a85
Tuğçe Ergün <tugceergun000@gmail.com>
2024-05-22 00:15:35 +0300

    Splash screen added.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -92,4 +92,11 @@ dependencies {
     kapt(libs.hilt.compiler)
     kapt(libs.hilt.androidx.compiler)
     implementation(libs.hilt.navigation.compose)
+
+    // Splash
+    implementation ("androidx.compose.ui:ui:1.4.2")
+    implementation ("androidx.compose.material:material:1.4.2")
+    implementation ("androidx.compose.ui:ui-tooling-preview:1.4.2")
+    implementation ("androidx.navigation:navigation-compose:2.5.3")
+    implementation ("com.airbnb.android:lottie-compose:6.0.0")
 }
