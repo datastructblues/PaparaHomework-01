f26e6eb40b714db213ef3720f8603cff5b3dc741
Demir <79748534+demirCann@users.noreply.github.com>
2024-05-22 04:29:36 +0300

    Add RemoteDataSource to define methods and add RemoteDataSourceImpl to handle API calls and provide data to repository

---
+++ app/src/main/java/com/example/hw01_papara/data/repository/RemoteDataSource.kt
@@ -0,0 +1,13 @@
+package com.example.hw01_papara.data.repository
+
+import com.example.hw01_papara.data.model.MealDetailResponse
+import com.example.hw01_papara.data.model.MealResponse
+import com.example.hw01_papara.utils.ApiResult
+import kotlinx.coroutines.flow.Flow
+
+interface RemoteDataSource {
+
+    suspend fun getMeals(): Flow<ApiResult<MealResponse>>
+
+    suspend fun getMealDetails(id: Int): Flow<ApiResult<MealDetailResponse>>
+}
