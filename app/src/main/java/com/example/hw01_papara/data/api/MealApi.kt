342a63d4bbd336ddd3624f558f0deb7f312e1aea
Demir <79748534+demirCann@users.noreply.github.com>
2024-05-22 04:26:31 +0300

    Add MealApi for fetching data and add ApiFlow to handle API responses and emit them as a stream

---
+++ app/src/main/java/com/example/hw01_papara/data/api/MealApi.kt
@@ -0,0 +1,27 @@
+package com.example.hw01_papara.data.api
+
+import com.example.hw01_papara.data.model.MealDetailResponse
+import com.example.hw01_papara.data.model.MealResponse
+import com.example.hw01_papara.utils.Constants.API_KEY
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface MealApi {
+
+    @GET("recipes/complexSearch")
+    suspend fun getAllMeals(
+        @Query("number") number: Int = 30,
+        @Query("offset") offset: Int = 0,
+        @Query("apiKey") apiKey: String = API_KEY
+    ): Response<MealResponse>
+
+
+    @GET("recipes/{id}/information")
+    suspend fun getMealDetails(
+        @Path("id") id: Int,
+        @Query("apiKey") apiKey: String = API_KEY
+    ): Response<MealDetailResponse>
+
+}
