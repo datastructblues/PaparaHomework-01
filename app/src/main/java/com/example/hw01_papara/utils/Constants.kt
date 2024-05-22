d45f95af01d36bb4124b9349af53b5b493c973b5
Demir <79748534+demirCann@users.noreply.github.com>
2024-05-22 04:42:57 +0300

    Add Constants object and formatSummary fun in util package

---
+++ app/src/main/java/com/example/hw01_papara/utils/Constants.kt
@@ -0,0 +1,11 @@
+package com.example.hw01_papara.utils
+
+object Constants {
+    const val BASE_URL = "https://api.spoonacular.com/"
+
+    const val API_KEY = "47cfb41863e042e6ad8f3076875f8d9e"
+
+    const val MEAL_ID_ARGUMENT_KEY = "mealId"
+
+
+}
