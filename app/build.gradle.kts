c6f26132cec2e0fb8046fdb215a41b2e06e181fc
Tuğçe Ergün <tugceergun000@gmail.com>
2024-05-22 12:02:31 +0300

    Register screen added.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -99,4 +99,9 @@ dependencies {
     implementation ("androidx.compose.ui:ui-tooling-preview:1.4.2")
     implementation ("androidx.navigation:navigation-compose:2.5.3")
     implementation ("com.airbnb.android:lottie-compose:6.0.0")
+
+    // Register
+    implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.5.1")
+    implementation ("androidx.activity:activity-compose:1.5.1")
+
 }
